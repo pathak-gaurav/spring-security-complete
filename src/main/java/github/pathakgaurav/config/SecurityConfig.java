6e81a8b784f0c2210115f22833ff068cfc9a003a
gauravpathak <gauravpathak.spring@gmail.com>
2021-03-11 22:27:20 -0500

    Application User without password encryption

---
+++ src/main/java/github/pathakgaurav/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package github.pathakgaurav.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails zack = User.builder().username("zack").password("{noop}password").roles("STUDENT").build();
+        return new InMemoryUserDetailsManager(zack);
+    }
+}
