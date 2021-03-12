0493865c4a22ac909cfa48c6976c7a474c1ddd14
gauravpathak <gauravpathak.spring@gmail.com>
2021-03-11 21:38:50 -0500

    Initial commit

---
+++ src/main/java/github/pathakgaurav/SpringSecurityCompleteApplication.java
@@ -0,0 +1,13 @@
+package github.pathakgaurav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCompleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityCompleteApplication.class, args);
+	}
+
+}
